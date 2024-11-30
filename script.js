0893fd58efe9a04d580cefea3a2a474b4526a0ee
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-11-30 15:11:49 +0530

    frontend

--- script.js
+++ script.js
@@ -1,47 +1,29 @@
-const Increment = document.getElementById("increment");
-const Decrement = document.getElementById("decrement");
-const Clear = document.getElementById("clear");
-
-const CountBtn = document.getElementById("CountBtn");
-const errorMessage = document.getElementById("errorMessage");
 
 
 
-let count = 0;
+let array = [
+    {
+        heading: "My Heading 1",
+        text: "This is some text 1"
+    },
+    {
+        heading: "My Heading 2",
+        text: "This is some text 2"
+    },
+    {
+        heading: "My Heading 3",
+        text: "This is some text 3"
+    },
+]
 
-function updatedCount() {
-    CountBtn.textContent = count;
 
-    if(count === 0) {
-        CountBtn.style.display = 'none'
-    } else {
-        CountBtn.style.display = 'inline-block'
-    }
-}
 
-Increment.addEventListener('click' , () => {
-    count++
-    updatedCount()
-
-});
-
-Decrement.addEventListener('click' , () => {
-    
-    if(count === 0) {
-        errorMessage.textContent = 'Cannot decrement count is already 0';
-        errorMessage.style.visibility = 'visible'
-    } else {
-        count--;
-        errorMessage.style.visibility = 'hidden';
-        updatedCount()
-    }
+array.forEach((item , i) => {
+    bodys.innerHTML +=
+    `
+     <div class="my-card">
+        <h1> ${item.heading} </h1>
+        <p> ${item.text} </p>
+     </div>
+    `
 })
-
-
-Clear.addEventListener('click' , () => {
-    count = 0
-    updatedCount()
-    errorMessage.style.visibility = 'hidden'
-})
-
-
